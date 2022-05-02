3fb93848d1f9c272910d49f2ec5041d34f375020
CelsoGallinari <102264454+CelsoGallinari@users.noreply.github.com>
2022-05-02 13:20:38 -0300

    Delete database.js

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize('fantasygames', 'root', '102030ju',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection
